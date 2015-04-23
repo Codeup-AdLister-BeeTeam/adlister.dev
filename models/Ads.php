7a53f4a3d841c8466e33a070248bdd266bb366f5
Paula C Schuler <paulasatx@gmail.com>
2015-04-23 11:26:12 -0500

    Yay! We have headline links on ads.index.php populating single ads on ads.show.php.
    We needed this for a good user experience.
    I also adjusted the H1 text on ads.show.php. Adusted slightly the Ads class.
    CSS was adjusted to enlarge font, change color and center the font. Readability is helped, but would be assisted more by a left flush margin a couple inches in on the page. Perhaps use a %em so it is still good on mobile.

--- models/Ads.php
+++ models/Ads.php
@@ -3,15 +3,13 @@
 
 require_once 'BaseModel.php';
 
-
-
 class Ads extends BaseModel {
 
 	protected static $table = 'ads';
 
 	protected function update()
     {
-        $query = "UPDATE contacts 
+        $query = "UPDATE ads 
         SET name = :name, email = :email, :phone, :houseNum, :street, :city, :state, :zipCode, :preferContact
         WHERE id = :id";
         $stmt = self::$dbc->prepare($query);
@@ -19,7 +17,6 @@ class Ads extends BaseModel {
         $stmt->bindValue(':id', 		$this->id, 				PDO::PARAM_STR);
         $stmt->bindValue(':headline', 	$this->headline, 		PDO::PARAM_STR);
         $stmt->bindValue(':price', 		$this->price, 			PDO::PARAM_STR);
-        // $stmt->bindValue(':size', 		$this->size, 			PDO::PARAM_STR);
         $stmt->bindValue(':category', 	$this->category, 		PDO::PARAM_STR);
         $stmt->bindValue(':description', $this->description, 	PDO::PARAM_STR);
         $stmt->bindValue(':contact', 	$this->contact, 		PDO::PARAM_STR);
@@ -30,15 +27,14 @@ class Ads extends BaseModel {
     }
     protected function insert()
      {   
-        $query = "INSERT INTO ads (id, headline, price, size, category, description, contact)
-        VALUES (:id, :headline, :price, :size, :cagetory, :description, :contact)";
+        $query = "INSERT INTO ads (id, headline, price, category, description, contact)
+        VALUES (:id, :headline, :price, :cagetory, :description, :contact)";
         
         $stmt = self::$dbc->prepare($query);
        
         $stmt->bindValue(':id', $this->id, PDO::PARAM_STR);
 		$stmt->bindValue(':headline', 	$this->headline, 		PDO::PARAM_STR);
         $stmt->bindValue(':price', 		$this->price, 			PDO::PARAM_STR);
-        $stmt->bindValue(':size', 		$this->size, 			PDO::PARAM_STR);
         $stmt->bindValue(':category', 	$this->category, 		PDO::PARAM_STR);
         $stmt->bindValue(':description', $this->description, 	PDO::PARAM_STR);
         $stmt->bindValue(':contact', 	$this->contact, 		PDO::PARAM_STR);
--- public/ads.index.php
+++ public/ads.index.php
@@ -4,40 +4,19 @@
 require '../bootstrap.php';
 
 
-$showAds = Ads::all();
 
-
-/*
- *The pageController function handles all processing for this page.
- *@return array an associative array of data used in rendering the HTML view.
- */
-// THAT CONTROLLER IS HOW WE LISTEN AND REACT TO THINGS, CLICKS ON ANCHOR TAGS BELOW
 function pageController()
 {       
-
-	// Initialize an empty data array.
-	$ads = [];
-
-	// Add data to be used in the HTML view.
-	// $data['message'] = 'Hello Keyasha!';
-
-	// if ($__GET['show'] == 'clicked')) {
-	
-
-	// }
-
-
-	// Return the completed data array.
-	return $ads;
-
+    // Step One in controller, initialize empty array
+    $adsForShow = ['showAds' => Ads::all()];
+    
+    return $adsForShow;   
 }
 
 extract(pageController());
 
-$ads = Ads::all();
 
 ?>
-
 
 
  <html>
@@ -58,28 +37,13 @@ $ads = Ads::all();
 <h1 class="ad-show">All Ad's</h1>
 <hr>
     <ul>
-		<?php foreach ($ads as $ad): ?>
+		<?php foreach ($showAds as $ad): ?>
 
-
-			<li><a href="ads.show.php?show=clicked=<?php echo $ad['id']?>" </a><?= $ad['headline']; ?></li>
-    
+			<li><a href="ads.show.php?id=<?=$ad['id']?>"</a><?= $ad['headline']; ?></li>
+            
         <?php endforeach; ?> 
 
-      <!--   <li><a href="ads.show.php">1 Relic Silver Ball Grommet Belt</a></li>
-        <li class="ad-show"><a id="ads-li" href="ads.show.php">2 Fun Shoe Ornaments</a></li>
-        <li class="ad-show"><a href="ads.show.php">3 Diamond Ring, 14 carat gold</a></li>
-        <li class="ad-show"><a href="ads.show.php">4 Heart and charm stones necklace</a></li> 
-        <li class="ad-show"><a href="ads.show.php">5 Fun shoes for a little girl</a></li> 
-        <li class="ad-show"><a href="ads.show.php">6 Cute fun party or play dress</a></li> 
-        <li class="ad-show"><a href="ads.show.php">7 LA Idol USA jeans, good condition</a></li> 
-        <li class="ad-show"><a href="ads.show.php">8 Vera Bradley purse, blue flower design</a></li> --> 
-         
-        
-
-
-
     </ul>
-
 
 
 <?php require_once '../views/partials/footer.php'; ?>
